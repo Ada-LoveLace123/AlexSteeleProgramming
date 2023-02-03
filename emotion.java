3e0351b9cf9e7ae07c94f29d392b8dca17b29ab8
Ada-LoveLace123 <117601660+Ada-LoveLace123@users.noreply.github.com>
2023-02-03 10:17:41 -0800

    Create emotion.java

---
+++ emotion.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class emotion
+{
+    public static void main(String[] args)
+    {
+        int happy = 0;
+        int sad = 0;
+        try 
+        {
+        File file = new File("happyorsad.txt");
+        Scanner SC = new Scanner(file);
+        String text = SC.nextLine();
+        for (String word: text.split(" "))
+        {
+            if (word.equals(":-("))
+            {
+                sad ++;
+            }
+            else if (word.equals(":-)"))
+            {
+                happy ++;
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            System.out.println("??? I dont see no file");
+        }
+        if (happy > sad){
+            System.out.println("happy");
+        }
+        else if (happy < sad){
+            System.out.println("sad");
+        }
+        else if (happy == sad){
+            System.out.println("neutral");
+        }
+        else{
+            System.out.println("none");
+        }
+        
+    }}
